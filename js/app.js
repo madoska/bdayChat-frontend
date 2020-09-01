d5fbcac7562aba40e8b9d52667ffe75d1c08edcc
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-09-01 06:46:10 +0200

    added check if token logged in

--- js/app.js
+++ js/app.js
@@ -1,35 +1,43 @@
-fetch('http://localhost:3000/views/chat', {
-    'headers': {
-        'Authorization': 'Bearer' + localStorage.getItem('token')
-    }
-}).then(result => {
-    return result.json();
-}).then(json => {
-    console.log(json)
-}).catch(err => {
-    console.log("Error")
-})
+window.addEventListener('load', (event) => {
+    let token = localStorage.getItem('token')
 
-document.querySelector('.input__btn').addEventListener('click', e => {
-    let text = document.querySelector('.input__text').value;
+    if(!token){
+        window.location.href="login.html"
+    }
 
     fetch('http://localhost:3000/views/chat', {
-        method: 'POST',
         'headers': {
-            'Content-type': 'application/json',
-            'Authorization': 'Bearer' + localStorage.getItem('token')
-        },
-        body: JSON.stringify({
-            'message': text
-        })
+            'Authorization': 'Bearer' + token
+        }
     }).then(result => {
         return result.json();
     }).then(json => {
-        if (json.status === 'success') {
-            console.log(text)
-        }
+        console.log(json)
     }).catch(err => {
-        console.log(err)
+        console.log("Error")
     })
-    e.preventDefault();
+    
+    document.querySelector('.input__btn').addEventListener('click', e => {
+        let text = document.querySelector('.input__text').value;
+    
+        fetch('http://localhost:3000/views/chat', {
+            method: 'POST',
+            'headers': {
+                'Content-type': 'application/json',
+                'Authorization': 'Bearer' + localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                'message': text
+            })
+        }).then(result => {
+            return result.json();
+        }).then(json => {
+            if (json.status === 'success') {
+                console.log(text)
+            }
+        }).catch(err => {
+            console.log(err)
+        })
+        e.preventDefault();
+    })
 })
