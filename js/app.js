cdbcca0b42a766327632fcc230551ed79ab47625
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-09-01 04:08:18 +0200

    function to save text on click (doesnt work)

--- js/app.js
+++ js/app.js
@@ -10,26 +10,26 @@ fetch('http://localhost:3000/views/chat', {
     console.log("Error")
 })
 
-let input = document.querySelector('#msg');
-input.addEventListener('keyup', e => {
-    if (e.keyCode === 13) {
-        let text = input.value;
-        fetch('http://localhost:3000/views/chat', {
-            method: 'post',
-            'headers': {
-                'Content-type': 'application/json',
-                'Authorization': 'Bearer' + localStorage.getItem('token')
-            },
-            body: JSON.stringify({
-                'text': text
-            })
-        }).then(result => {
-            return result.json();
-        }).then(json => {
-            console.log(json)
-        }).catch(err => {
-            console.log(err)
+document.querySelector('.input__btn').addEventListener('click', e => {
+    let text = document.querySelector('.input__text').value;
+
+    fetch('http://localhost:3000/views/chat', {
+        method: 'POST',
+        'headers': {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer' + localStorage.getItem('token')
+        },
+        body: JSON.stringify({
+            'message': text
         })
-    }
+    }).then(result => {
+        return result.json();
+    }).then(json => {
+        if (json.status === 'success') {
+            console.log(text)
+        }
+    }).catch(err => {
+        console.log(err)
+    })
     e.preventDefault();
 })
